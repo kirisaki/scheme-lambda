35046da959a11e1ab4663f642acb7e2c0b1074b1
Akihito Kirisaki <kirisaki@klara.works>
2021-07-08 14:17:30 +0900

    feat: free variable

--- lambda.scm
+++ lambda.scm
@@ -1,4 +1,5 @@
 (use gauche.record)
+(use gauche.collection)
 
 (define-record-type Var (var n) var? (n var-name))
 (define-record-type Lam (lam n e) lam? (n lam-name) (e lam-expr))
@@ -11,6 +12,15 @@
    ((var? e) (let ((n (var-name e))) (string-append "(Var " (name n)  ")"))) 
    ((lam? e) (let ((n (lam-name e)) (e (lam-expr e))) (string-append "(Lam " (name n) " " (show e) ")")))
    ((app? e) (let ((f (app-func e)) (e (app-expr e))) (string-append "(App " (show f) " " (show e) ")")))
-   (else (error "Unexpected expr."))
+   (else (error "Unexpected expr at show."))
    )
   )
+
+(define (free e)
+  (cond
+   ((var? e) (list (var-name e)))
+   ((lam? e) (let ((ns (free (lam-expr e)))) (remove (lambda (n) (eq? n (lam-name e))) ns)))
+   ((app? e) (let ((nsf (free (app-func e))) (nse (free (app-expr e)))) (concatenate (list nsf nse))))
+   (else (error "Unexpected expr at free."))
+   )
+)
